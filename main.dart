36a8fc51d7b6a2d00cb1f8b7d090feafc514c86d
Code <59381987+c-o-de@users.noreply.github.com>
2023-08-22 08:54:57 +0600

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,13 @@
+// This is a comment in Dart. It's ignored by the Dart compiler and used for documentation.
+
+// Define a function that prints a greeting message
+void greet(String name) {
+  print("Hello, $name!");
+}
+
+// Define the main function, where the execution starts
+void main() {
+  // Call the greet function
+  greet("Alice");
+  greet("Bob");
+}
